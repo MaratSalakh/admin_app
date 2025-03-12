033d55f10e46a29661888f56c3154a668195a525
Marat Salakh <marat.salakh.dev@gmail.com>
2025-03-12 18:45:55 +0300

    modify InfoPage

--- src/pages/InfoPage/InfoPage.vue
+++ src/pages/InfoPage/InfoPage.vue
@@ -1,5 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useWorksStore } from "../../app/store/store.ts";
 
-<template><div>hello!</div></template>
+const store = useWorksStore();
+</script>
+
+<template>
+    <div>{{ store.data?.currentCity }}</div>
+    <div>{{ store.data?.currentWorkshop }}</div>
+    <div>{{ store.data?.currentEmployee }}</div>
+    <div>{{ store.data?.currentBrigade }}</div>
+    <div>{{ store.data?.currentShift }}</div>
+</template>
 
 <style scoped></style>
